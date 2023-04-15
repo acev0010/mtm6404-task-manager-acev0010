7efbc8aec8ee3e800a81c0d61eab972df9bcc8e8
Isabel Acevedo <acev0010@algonquinlive.com>
2023-04-15 18:08:04 -0400

    route2

--- App.jsx
+++ App.jsx
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
@@ -5,50 +7,46 @@ import TaskList from "./components/TaskList";
 import Warning from "./components/Warning";
 import Logo from "./components/Logo";
 
-
+import { BrowserRouter as Router } from "react-router-dom";
 
 function Time(props) {
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
-    
-    if (hours < 12) {
-        timeOfDay = "morning"
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = "afternoon"
-    } else {
-        timeOfDay = "night"
-    }
-    
-    return (
-        <div>
-        <h1>Good {timeOfDay} {props.user}!</h1>
-        {props.children}
-        </div>
-    )
-}
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
 
+  if (hours < 12) {
+    timeOfDay = "morning";
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "night";
+  }
+
+  return (
+    <div>
+      <h1>
+        Good {timeOfDay} {props.user}!
+      </h1>
+      {props.children}
+    </div>
+  );
+}
 
 export default function App() {
-    return (
-        <div className="container">
-            <div className="header">
-            <Logo />
-            
-            <Time user="Isabel">
-                <p> Please check all the tasks below </p>  
-            </Time>
-            </div>
-            <Navigation />
-            <TaskList />
-            <Warning 
-            course="Motion Graphics II"
-            overdue="Overdue"
-            />
-            <Footer />
+  return (
+    <Router>
+      <div className='container'>
+        <div className='header'>
+          <Logo />
+          <Time user='Isabel'>
+            <p> Please check all the tasks below </p>
+          </Time>
         </div>
-    )
+        <Navigation />
+        <TaskList />
+        <Warning course='Motion Graphics II' overdue='Overdue' />
+        <Footer />
+      </div>
+    </Router>
+  );
 }
-
-
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
-import School from "../components/School";
-import {createBrowserRouter, RouterProvider,} from 'react-router-dom'
+import School from './School';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "/school",
-    element: <School />
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/school" component={School} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
+  document.getElementById('root')
 );
